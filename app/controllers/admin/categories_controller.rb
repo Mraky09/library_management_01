7de17fe81ac9e2a9ecf40f2055efa730159c969f
Tuan Anh <anhtnt09@gmail.com>
2016-12-22 01:18:19 +0700

    Edit category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
   before_action :verify_login, :verify_admin
+  before_action :load_category, except: [:create, :index, :new]
+
   def index
     @categories = Category.search(params[:search]).order(created_at: :DESC).
       paginate page: params[:page], per_page: Settings.item_per_page
@@ -10,5 +12,25 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def show
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = t ".edit_category"
+      render :edit
+    else
+      flash[:danger] = t ".edit_failed"
+      render :edit
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit :name, :description
+  end
+
+  def load_category
+    @category = Category.find_by id: params[:id]
+    render_404 unless @category
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :books
-
+  validates :name, presence: true, length: {maximum: 255}, uniqueness:
+    {case_sensitive: false}
   scope :search, -> (condition) {where("name LIKE :search OR description LIKE :search",
     search: "%#{condition}%")}
 end
