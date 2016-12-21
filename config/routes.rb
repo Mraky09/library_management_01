1e62d89df3543635c88e2ff98a05dad2b165aa14
Mraky09 <anhtnt09@gmail.com>
2016-12-21 09:11:07 +0700

    User signup & logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  delete "/logout", to: "sessions#destroy"
 end
